faabbd1d3182e132e84f04b275b56541fafdf4a4
Амина Кадралинова <Tyuiu.KadralinovaAT@outlook.com>
2025-10-27 20:35:29 +0500

    Task4

---
+++ Tyuiu.KadralinovaAT.Sprint3.Task4.V30.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.KadralinovaAT.Sprint3.Task4.V30.Lib;
+
+namespace Tyuiu.KadralinovaAT.Sprint3.Task4.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            double res = ds.Calculate(startValue, stopValue);
+
+            double wait = 1.924;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KadralinovaAT.Sprint3.Task4.V30/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.KadralinovaAT.Sprint3.Task4.V30.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #3 | Выполнила: Кадралинова А. Т. | ПИНб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #3                                                               *");
+Console.WriteLine("* Тема: Операторы continue и break                                        *");
+Console.WriteLine("* Задание #4                                                              *");
+Console.WriteLine("* Вариант #30                                                             *");
+Console.WriteLine("* Выполнила: Кадралинова Амина Тастемировна | ПИНб-25-1                   *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая вычисляет значение функции                  *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int startValue = -5;
+int stopValue = 5;
+
+Console.WriteLine("Старт шага = " + startValue);
+Console.WriteLine("Конец шага = " + stopValue);
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Сумма ряда = " + ds.Calculate(startValue, stopValue));
+
+Console.ReadKey();
