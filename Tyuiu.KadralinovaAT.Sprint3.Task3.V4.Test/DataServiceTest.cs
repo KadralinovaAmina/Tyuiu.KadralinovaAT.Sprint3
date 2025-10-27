0c894fe1fcc9a567fd11b7bda29e9cfaabcb4f2b
Амина Кадралинова <Tyuiu.KadralinovaAT@outlook.com>
2025-10-27 20:05:51 +0500

    Task3

---
+++ Tyuiu.KadralinovaAT.Sprint3.Task3.V4.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.KadralinovaAT.Sprint3.Task3.V4.Lib;
+
+namespace Tyuiu.KadralinovaAT.Sprint3.Task3.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidDeleteCharInString()
+        {
+            DataService ds = new DataService();
+            string str = "plkjjdw cvjkl";
+            char chr = 'j';
+
+            string res = ds.DeleteCharInString(str, chr);
+
+            string wait = "plkdw cvkl";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KadralinovaAT.Sprint3.Task3.V4/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.KadralinovaAT.Sprint3.Task3.V4.Lib;
+
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #3 | Выполнила: Кадралинова А. Т. | ПИНб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #3                                                               *");
+Console.WriteLine("* Тема: Оператор цикла foreach                                            *");
+Console.WriteLine("* Задание #3                                                              *");
+Console.WriteLine("* Вариант #4                                                              *");
+Console.WriteLine("* Выполнила: Кадралинова Амина Тастемировна | ПИНб-25-1                   *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая вычисляет сумму                             *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+string value = "plkjjdw cvjkl";
+char chr = 'j';
+
+Console.WriteLine("Исходная строка = " + value);
+Console.WriteLine("Искомый символ = " + chr);
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Измененная строка = " + ds.DeleteCharInString(value, chr));
+
+Console.ReadKey();
